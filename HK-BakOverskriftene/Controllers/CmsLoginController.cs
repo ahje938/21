cbc37e180b3af6b6b2166fff5c34fe0cb8dc64bd
HansRisberg <123938027+HansRisberg@users.noreply.github.com>
2024-12-29 13:50:00 +0100

    Hiding password and username in env variables

--- HK-BakOverskriftene/Controllers/CmsLoginController.cs
+++ HK-BakOverskriftene/Controllers/CmsLoginController.cs
@@ -1,29 +1,51 @@
-﻿using BakOverskriftene.Domain.Models.DTO_s;
+﻿//using BakOverskriftene.Domain.Models.DTO_s;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace HK_BakOverskriftene.Controllers {
+
+//    [Route("api/[controller]")]
+//    [ApiController]
+
+//    public class CmsLoginController : ControllerBase {
+
+//        /// <summary>
+//        /// Hardcoded the username and password. Will need to put credentials in the database at some point. 
+//        /// </summary>
+//        /// <param name="login"></param>
+//        /// <returns></returns>
+//        [HttpPost("login")]
+//        public IActionResult Login([FromBody] CmsLoginDTO login) {
+//            // Hardcoded credentials
+//            const string hardcodedUsername = "admin";
+//            const string hardcodedPassword = "password123";
+
+//            if (login.Username == hardcodedUsername && login.Password == hardcodedPassword) {
+//                return Ok(new { message = "Login successful" });
+//            }
+
+//            return Unauthorized(new { message = "Invalid username or password" });
+//        }
+//    }
+//}
+using BakOverskriftene.Domain.Models.DTO_s;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HK_BakOverskriftene.Controllers {
+/// <summary>
+/// Hardcoded the username and password. Will need to put credentials in the database at some point. 
+/// </summary>
+/// <param name="login"></param>
+/// <returns></returns>
+public class CmsLoginController : ControllerBase {
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] CmsLoginDTO login) {
+        // Fetch credentials from environment variables
+        var username = Environment.GetEnvironmentVariable("CMS_LOGIN_USERNAME");
+        var password = Environment.GetEnvironmentVariable("CMS_LOGIN_PASSWORD");
 
-    [Route("api/[controller]")]
-    [ApiController]
-
-    public class CmsLoginController : ControllerBase {
-
-        /// <summary>
-        /// Hardcoded the username and password. Will need to put credentials in the database at some point. 
-        /// </summary>
-        /// <param name="login"></param>
-        /// <returns></returns>
-        [HttpPost("login")]
-        public IActionResult Login([FromBody] CmsLoginDTO login) {
-            // Hardcoded credentials
-            const string hardcodedUsername = "admin";
-            const string hardcodedPassword = "password123";
-
-            if (login.Username == hardcodedUsername && login.Password == hardcodedPassword) {
-                return Ok(new { message = "Login successful" });
-            }
-
-            return Unauthorized(new { message = "Invalid username or password" });
+        if (login.Username == username && login.Password == password) {
+            return Ok(new { message = "Login successful" });
         }
+
+        return Unauthorized(new { message = "Invalid username or password" });
     }
 }
