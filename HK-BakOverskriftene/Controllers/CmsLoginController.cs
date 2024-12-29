fb0414e27a45f08f033d06f6060abf178ed7d6a1
HansRisberg <123938027+HansRisberg@users.noreply.github.com>
2024-12-29 14:35:50 +0100

    removing unused code

--- HK-BakOverskriftene/Controllers/CmsLoginController.cs
+++ HK-BakOverskriftene/Controllers/CmsLoginController.cs
@@ -1,33 +1,4 @@
-﻿//using BakOverskriftene.Domain.Models.DTO_s;
-//using Microsoft.AspNetCore.Mvc;
-
-//namespace HK_BakOverskriftene.Controllers {
-
-//    [Route("api/[controller]")]
-//    [ApiController]
-
-//    public class CmsLoginController : ControllerBase {
-
-//        /// <summary>
-//        /// Hardcoded the username and password. Will need to put credentials in the database at some point. 
-//        /// </summary>
-//        /// <param name="login"></param>
-//        /// <returns></returns>
-//        [HttpPost("login")]
-//        public IActionResult Login([FromBody] CmsLoginDTO login) {
-//            // Hardcoded credentials
-//            const string hardcodedUsername = "admin";
-//            const string hardcodedPassword = "password123";
-
-//            if (login.Username == hardcodedUsername && login.Password == hardcodedPassword) {
-//                return Ok(new { message = "Login successful" });
-//            }
-
-//            return Unauthorized(new { message = "Invalid username or password" });
-//        }
-//    }
-//}
-using BakOverskriftene.Domain.Models.DTO_s;
+﻿using BakOverskriftene.Domain.Models.DTO_s;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HK_BakOverskriftene.Controllers {
